dd4909a40807d1367726267d4807b98ede6bf2d0
Ritik24307 <ritik1314raushan@gmail.com>
2026-09-11 14:01:43 +0530

    Add Session 3 OOP practice problems

---
+++ IdCardDemo.java
@@ -0,0 +1,39 @@
+class IdCard {
+
+    String name;
+    int booksIssued;
+
+    // Constructor
+    IdCard(String name, int booksIssued) {
+        this.name = name;
+        this.booksIssued = booksIssued;
+    }
+}
+
+class IdCardDemo {
+
+    public static void main(String[] args) {
+
+        // Create one object
+        IdCard ravi = new IdCard("Ravi", 0);
+
+        // duplicate points to the SAME object
+        IdCard duplicate = ravi;
+
+        // Change through duplicate
+        duplicate.booksIssued = 3;
+
+        // Create a separate object
+        IdCard separate = new IdCard("Ravi", 3);
+
+        // Print results
+        System.out.println(
+            "Ravi's booksIssued (via first variable): "
+            + ravi.booksIssued
+        );
+
+        System.out.println("duplicate == ravi: " + (duplicate == ravi));
+
+        System.out.println("separate == ravi: " + (separate == ravi));
+    }
+}
