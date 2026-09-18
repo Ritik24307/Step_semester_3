5d3c4182895d51a53357229ebc93d20bf8670dc1
Ritik24307 <ritik1314raushan@gmail.com>
2026-09-18 20:15:41 +0530

    Restored week 5 assignment and week 4 programs

---
+++ week 4 ap/PayRollBonus.java
@@ -0,0 +1,36 @@
+class Employee {
+    String employeeId;
+    double salary;
+
+    public Employee(String employeeId, double salary) {
+        this.employeeId = employeeId;
+        this.salary = salary;
+    }
+
+    public void raiseSalary(double salary) {
+        this.salary = this.salary + salary;
+    }
+
+    public void printSalary() {
+        System.out.println(
+            employeeId + " | Final Salary: Rs " + salary
+        );
+    }
+}
+
+public class PayRollBonus {
+    public static void main(String[] args) {
+
+        Employee[] employees = {
+            new Employee("E-101", 40000),
+            new Employee("E-102", 55000),
+            new Employee("E-103", 62000),
+            new Employee("E-104", 48000)
+        };
+
+        for (int i = 0; i < employees.length; i++) {
+            employees[i].raiseSalary(5000);
+            employees[i].printSalary();
+        }
+    }
+}
---
+++ week 5 assignment problem/LibraryMemberBean.java
@@ -0,0 +1,65 @@
+class LibraryMember {
+
+    private String membershipId;
+    private String name;
+    private boolean premiumMember;
+    private String securityAnswer;
+
+    public LibraryMember() {
+        premiumMember = false;
+    }
+
+    public String getMembershipId() {
+        return membershipId;
+    }
+
+    public void setMembershipId(String id) {
+        if (membershipId == null) {
+            membershipId = id;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isPremiumMember() {
+        return premiumMember;
+    }
+
+    public void setPremiumMember(boolean premium) {
+        this.premiumMember = premium;
+    }
+
+    public void setSecurityAnswer(String answer) {
+        securityAnswer = Integer.toHexString(answer.hashCode());
+    }
+}
+
+public class LibraryMemberBean {
+
+    public static void main(String[] args) {
+
+        LibraryMember m = new LibraryMember();
+
+        m.setMembershipId("LIB-8841");
+        m.setName("Priya Nair");
+        m.setPremiumMember(true);
+
+        System.out.println(m.getMembershipId());
+        System.out.println(m.getName());
+        System.out.println(m.isPremiumMember());
+
+        m.setMembershipId("FAKE-0000");
+
+        System.out.println(m.getMembershipId());
+
+        m.setSecurityAnswer("BlueMountain");
+
+        System.out.println("Security answer stored");
+    }
+}
