c34357b0c124085aedc2dbe2661fb9cc4611ac16
Ritik24307 <ritik1314raushan@gmail.com>
2026-09-18 17:05:13 +0530

    week 4 practice problem

---
+++ AccountPayments.java
@@ -0,0 +1,55 @@
+class FeeAccount {
+    public void pay(double amount) {
+        System.out.println("Paid in one go (day-scholar account)");
+    }
+}
+
+class HostelFeeAccount extends FeeAccount {
+    public void payInTwoInstallments(double amount) {
+        System.out.println("Paid in two installments (hostel account)");
+    }
+}
+
+public class AccountPayments {
+
+    static int hostelCount = 0;
+    static int dayScholarCount = 0;
+
+    public static void processPayment(FeeAccount account, double amount) {
+
+        if (account instanceof HostelFeeAccount) {
+            HostelFeeAccount hostel = (HostelFeeAccount) account;
+            hostel.payInTwoInstallments(amount);
+            hostelCount++;
+        } else {
+            account.pay(amount);
+            dayScholarCount++;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        FeeAccount[] accounts = {
+            new HostelFeeAccount(),
+            new HostelFeeAccount(),
+            new FeeAccount(),
+            new FeeAccount()
+        };
+
+        double[] amounts = {
+            60000,
+            60000,
+            60000,
+            60000
+        };
+
+        for (int i = 0; i < accounts.length; i++) {
+            processPayment(accounts[i], amounts[i]);
+        }
+
+        System.out.println(
+            "Hostel accounts processed: " + hostelCount +
+            " | Day-scholar accounts processed: " + dayScholarCount
+        );
+    }
+}
